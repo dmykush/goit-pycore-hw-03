c1fce4d7fe4e6589af67404b0041c236cf06442c
dmykush <kushnir@myyahoo.com>
2025-06-16 23:15:48 +0200

    c_2

--- Task_3.py
+++ Task_3.py
@@ -4,11 +4,11 @@ def edited_num(Phone_number):
     clean_num = re.sub(r'[^\d+]',  '',Phone_number)
     if clean_num.startswith('+'):
         return print(clean_num)
-    elif clean_num.startswith('38') and not clean_num.startswith('+380'):
-        return print("+380" + clean_num[3:])
     elif clean_num.startswith('380'):
         return print("+" + clean_num)
-    else:
+    elif clean_num.startswith('0'):
         return print('+38' + clean_num)
+    else:
+        return print('+380' + clean_num)
     
-edited_num('3516100203')
+edited_num('0674013510')
